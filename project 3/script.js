2d47f5479e9635572fd663bcdf626c4513a8a126
jainchirag <jainc3620@gmail.com>
2025-08-14 17:33:11 +0530

    Delete project 3 directory

--- project 3/script.js
+++
@@ -1,8 +0,0 @@
-const clock = document.getElementById('clock') // ye bhi use kr skte h 
-// const clock = document.querySelector('#clock') // ye bhi 
-
-setInterval(function() {
-let date = new Date();
-// console.log(date.toLocaleTimeString);
-clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
